3ee3535772284eed882cc15f1b2c48658162454f
Phuoc Nguyen <phuocnguyen@macs-MacBook-Pro.local>
2020-07-23 10:01:19 +0200

    fixed r-enc cons. and decons. rules

--- lib/theory/src/Theory/Tools/IntruderRules.hs
+++ lib/theory/src/Theory/Tools/IntruderRules.hs
@@ -316,11 +316,17 @@ mkDUnionRule t_prems t_conc =
 
 randIntruderRules ::  [IntrRuleAC]
 randIntruderRules = [mkCRPkRule x_var rpk_x,
-                     mkCREncRule y_var z_var x_var renc_y_z_rpkx,
+                     
+                     mkCREncRule y_var z_var x_var renc_y_z_x,
                      mkCRDecRule y_var x_var rdec_y_skx,
-                     mkCRRandRule y_var z_var x_var rrand_y_z_rpkx,
-                     mkDRDecRule y_var z_var x_var y_var,
-                     mkDRRandRule y_var z_var x_var t_var renc_y_zt_rpkx]
+                     mkCRRandRule y_var z_var x_var rrand_y_z_x,
+                     
+                     mkDREncRule y_var z_var x_var renc_y_z_rpkx,
+                     mkDRDecRule y_var x_var rdec_y_skx,
+                     mkDRRandRule y_var z_var x_var rrand_y_z_rpkx,
+
+                     mkDRDecRule2 y_var z_var x_var y_var,
+                     mkDRRandRule2 y_var z_var rpk_x t_var renc_y_zt_rpkx]
     where x_var = varTerm (LVar "x" LSortMsg 0)
           y_var = varTerm (LVar "y" LSortMsg 0)
           z_var = varTerm (LVar "z" LSortMsg 0)
@@ -328,16 +334,18 @@ randIntruderRules = [mkCRPkRule x_var rpk_x,
           rpk_x = fAppNoEq rpkSym [x_var]
           zt_radd = fAppAC Radd [z_var, t_var]
           renc_y_z_rpkx = fAppNoEq rencSym [y_var, z_var, rpk_x]
+          renc_y_z_x = fAppNoEq rencSym [y_var, z_var, x_var]
           rdec_y_skx = fAppNoEq rdecSym [y_var, x_var]
           rrand_y_z_rpkx = fAppNoEq rrandSym [y_var, z_var, rpk_x]
+          rrand_y_z_x = fAppNoEq rrandSym [y_var, z_var, x_var]
           renc_y_zt_rpkx = fAppNoEq rencSym [y_var, zt_radd, rpk_x]
                        -- TODO: add constructor rules for all functions (renc, rdec, rpk, rrand)
                        --       add two deconstruction rules: one for rdec(renc(...)) (unlimited applications)
                        --                               , and one for rrand(renc(...)) (limited to one application)
 
 -- rrand(renc(m, r1, pk(sk)), r2, pk(sk)) = renc(m, radd(r1, r2), pk(sk))
-mkDRRandRule :: LNTerm -> LNTerm -> LNTerm -> LNTerm -> LNTerm -> IntrRuleAC
-mkDRRandRule t_prem t_prem2 t_prem3 t_prem4 t_conc =
+mkDRRandRule2 :: LNTerm -> LNTerm -> LNTerm -> LNTerm -> LNTerm -> IntrRuleAC
+mkDRRandRule2 t_prem t_prem2 t_prem3 t_prem4 t_conc =
     Rule (DestrRule (append (pack "_") rrandSymString) 1 True False)
          [kdFact $ fAppNoEq rencSym [t_prem, t_prem2, t_prem3],
           kuFact t_prem3, kuFact t_prem4]
@@ -345,8 +353,8 @@ mkDRRandRule t_prem t_prem2 t_prem3 t_prem4 t_conc =
 
 -- rdec(renc(m, r, pk(sk)), sk) = m
 -- (m, r, sk)
-mkDRDecRule :: LNTerm -> LNTerm -> LNTerm -> LNTerm -> IntrRuleAC
-mkDRDecRule t_prem t_prem2 t_prem3 t_conc =
+mkDRDecRule2 :: LNTerm -> LNTerm -> LNTerm -> LNTerm -> IntrRuleAC
+mkDRDecRule2 t_prem t_prem2 t_prem3 t_conc =
     Rule (DestrRule (append (pack "_") rdecSymString) 0 True False)
          [kdFact $ fAppNoEq rencSym [t_prem, t_prem2, t_prem3_pk], kuFact t_prem3]
          [kdFact t_conc] [] []
@@ -362,20 +370,39 @@ mkCRPkRule t_prem t_conc =
 mkCREncRule :: LNTerm -> LNTerm -> LNTerm -> LNTerm -> IntrRuleAC
 mkCREncRule t_prem t_prem2 t_prem3 t_conc =
     Rule (ConstrRule (append (pack "_") rencSymString))
-         [kuFact t_prem, kuFact t_prem2, kuFact $ fAppNoEq rpkSym [t_prem3]]
+         [kuFact t_prem, kuFact t_prem2, kuFact t_prem3]
          [kuFact t_conc] [kuFact t_conc] []
+
+-- KU(m), KU(r), KD(pk(sk)) -> KD renc(m, r, pk(sk))
+mkDREncRule :: LNTerm -> LNTerm -> LNTerm -> LNTerm -> IntrRuleAC
+mkDREncRule t_prem t_prem2 t_prem3 t_conc =
+    Rule (DestrRule (append (pack "_") rencSymString) 0 True False)
+         [kuFact t_prem, kuFact t_prem2, kdFact $ fAppNoEq rpkSym [t_prem3]]
+         [kdFact t_conc] [] []
 
 mkCRRandRule :: LNTerm -> LNTerm -> LNTerm -> LNTerm -> IntrRuleAC
 mkCRRandRule t_prem t_prem2 t_prem3 t_conc =
     Rule (ConstrRule (append (pack "_") rrandSymString))
-         [kuFact t_prem, kuFact t_prem2, kuFact $ fAppNoEq rpkSym [t_prem3]]
+         [kuFact t_prem, kuFact t_prem2, kuFact t_prem3]
          [kuFact t_conc] [kuFact t_conc] []
+
+mkDRRandRule :: LNTerm -> LNTerm -> LNTerm -> LNTerm -> IntrRuleAC
+mkDRRandRule t_prem t_prem2 t_prem3 t_conc =
+    Rule (DestrRule (append (pack "_") rrandSymString) 0 True False)
+         [kuFact t_prem, kuFact t_prem2, kdFact $ fAppNoEq rpkSym [t_prem3]]
+         [kuFact t_conc] [] []
 
 mkCRDecRule :: LNTerm -> LNTerm -> LNTerm -> IntrRuleAC
 mkCRDecRule t_prem t_prem2 t_conc =
     Rule (ConstrRule (append (pack "_") rdecSymString))
          [kuFact t_prem, kuFact t_prem2]
          [kuFact t_conc] [kuFact t_conc] []
+
+mkDRDecRule :: LNTerm -> LNTerm -> LNTerm -> IntrRuleAC
+mkDRDecRule t_prem t_prem2 t_conc =
+    Rule (DestrRule (append (pack "_") rdecSymString) 0 True False)
+         [kuFact t_prem, kdFact $ fAppNoEq rpkSym [t_prem2]]
+         [kdFact t_conc] [] []
 
 ------------------------------------------------------------------------------
 -- Xor intruder rules
